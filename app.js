79148bf40262237a087fd0dadc69628ea451b61c
rajanmah <146156379+rajanmah@users.noreply.github.com>
2023-10-27 12:59:21 -0400

    first HTML Skeleton

---
+++ app.js
@@ -0,0 +1,22 @@
+// document.body.style.backgroundColor = "gray";
+
+// let subMenu=document.getElementbyId("submenu");
+// let h2=document.createElement("h2");
+// h2.appendChild(document.createTextNode("heading TOOO"));
+// subMenu.appendChild(h2)
+
+
+
+// let ul=document.createElement("ul").classList.add("ulsubmenu");
+// let ulSubMenu=document.querySelector("ulsubmenu");
+
+// ulSubMenu.appendChild(document.createElement("li"))
+
+var h1 = document.createElement('h1');
+
+// Set the text for the heading
+h1.appendChild(document.createTextNode('Heading Text'));
+
+// Append the heading to the body or any other element in the DOM
+document.body.appendChild(h1);
+
