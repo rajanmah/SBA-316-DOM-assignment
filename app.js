f74abbf589a6591058336b0c1dc50f8a8d3e95f7
rajanmah <146156379+rajanmah@users.noreply.github.com>
2023-10-28 21:54:23 -0400

    events added

--- app.js
+++ app.js
@@ -1,22 +1,194 @@
+// import "./style.css";
+
+
 // document.body.style.backgroundColor = "gray";
 
 // let subMenu=document.getElementbyId("submenu");
-// let h2=document.createElement("h2");
-// h2.appendChild(document.createTextNode("heading TOOO"));
-// subMenu.appendChild(h2)
+
+// let subLink = document.createElement("a");
+// let textNode = document.createTextNode("Nepalese");
+// subLink.appendChild(textNode);
+// subLink.href="https:/www.gmail.com";
+// document.body.appendChild(textNode);
+
+    //   var a = document.createElement('a');
+    //   var linkText = document.createTextNode("Nepalese");
+    //   a.appendChild(linkText);
+    //   a.classList.add("menu")
+    //   a.title = "my title text";
+    //   a.href = "http://example.com";
+    //   document.body.appendChild(a);
+
+    //   var a = document.createElement('a');
+    //   var linkText = document.createTextNode("Indian");
+    //   a.appendChild(linkText);
+    //   a.classList.add("menu")
+    //   a.title = "my title text";
+    //   a.href = "http://example.com";
+    //   document.body.appendChild(a);
 
 
+    //   var a = document.createElement('a');
+    //   var linkText = document.createTextNode("Chinese");
+    //   a.appendChild(linkText);
+    //   a.classList.add("menu")
+    //   a.title = "my title text";
+    //   a.href = "http://example.com";
+    //   document.body.appendChild(a);
 
-// let ul=document.createElement("ul").classList.add("ulsubmenu");
-// let ulSubMenu=document.querySelector("ulsubmenu");
 
-// ulSubMenu.appendChild(document.createElement("li"))
+    
+const subMenu = [
+    { text: "Nepalese  ", href: "#h1" },
+    { text: "Indian  ", href: "#h2" },
+    { text: "Chinese  ", href: "#h3" },
+    { text: "Korean  ", href: "/account" }
+  ];
 
-var h1 = document.createElement('h1');
+    const subMenuBar = document.querySelector("#submenu");
 
-// Set the text for the heading
-h1.appendChild(document.createTextNode('Heading Text'));
+    for (const link of subMenu) {
+        const aElement = document.createElement("a");
+        // aElement.style.margin="10px";
+        aElement.classList.add("menu")
+        aElement.textContent = link.text;
+        aElement.href = link.href;
+        subMenuBar.appendChild(aElement);
+      }
 
-// Append the heading to the body or any other element in the DOM
-document.body.appendChild(h1);
 
+      // sign up form
+      const form = document.getElementById("form");
+const name = form.elements["name"];
+const email = form.elements["email"];
+const zip = form.elements["zip"];
+const country = form.elements["country"];
+const password = form.elements["password"];
+
+form.addEventListener("submit", validate);
+
+// The big validation function.
+// While this may seem like a lot of functions we
+// need to build, most will be very simple!
+function validate(evt) {
+  const nameVal = validateName();
+  if (nameVal === false) {
+    evt.returnValue = false;
+    return false;
+  }
+
+  const emailVal = validateEmail();
+  if (emailVal === false) {
+    evt.returnValue = false;
+    return false;
+  }
+
+  const zipVal = validateZip();
+  if (zipVal === false) {
+    evt.returnValue = false;
+    return false;
+  }
+
+  const countryVal = validateCountry();
+  if (countryVal === false) {
+    evt.returnValue = false;
+    return false;
+  }
+
+  const passwordVal = validatePassword();
+  if (passwordVal === false) {
+    evt.returnValue = false;
+    return false;
+  }
+
+  alert(`Name: ${nameVal}
+Email: ${emailVal}
+Country: ${countryVal}
+Zip Code: ${zipVal}
+Password: ...that's a secret.`);
+
+  return true;
+}
+
+// Simple email validation.
+// We added a check to make sure the name exists!
+// Note that this could also be solved via adding
+// the "required" attribute to the HTML, but the
+// focus of this activity is DOM events.
+function validateEmail() {
+  let emailVal = email.value;
+
+  if (emailVal === "") {
+    alert("Please provide an email.");
+    email.focus();
+    return false;
+  }
+
+  const atpos = emailVal.indexOf("@");
+  const dotpos = emailVal.lastIndexOf(".");
+
+  if (atpos < 1) {
+    alert(
+      "Your email must include an @ symbol which must not be at the beginning of the email."
+    );
+    email.focus();
+    return false;
+  }
+
+  if (dotpos - atpos < 2) {
+    alert(
+      "Invalid structure: @.\nYou must include a domain name after the @ symbol."
+    );
+    email.focus();
+    return false;
+  }
+
+  return emailVal;
+}
+
+// Name Validation
+function validateName() {
+  if (name.value === "") {
+    alert("Please provide a name.");
+    name.focus();
+    return false;
+  }
+  return name.value;
+}
+
+// Password Validation
+function validatePassword() {
+  if (password.value === "") {
+    alert("Please provide a password.");
+    password.focus();
+    return false;
+  }
+  return password.value;
+}
+
+// Country Validation
+function validateCountry() {
+  if (country.value === "") {
+    alert("Please provide a country.");
+    country.focus();
+    return false;
+  }
+  return country.value;
+}
+
+// Zip Code Validation
+function validateZip() {
+  if (zip.value === "") {
+    alert("Please provide a zip code.");
+    zip.focus();
+    return false;
+  }
+
+  if (zip.value.length !== 5 || isNaN(Number(zip.value))) {
+    alert("Zip codes must be in the format #####.");
+    zip.focus();
+    return false;
+  }
+
+  return zip.value;
+}
